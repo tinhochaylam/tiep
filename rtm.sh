550531e93f1d611f774efe10aacea9ea9c59eaf8
tinhochaylam <95688598+tinhochaylam@users.noreply.github.com>
2021-12-07 21:42:23 -0800

    Update rtm.sh

--- rtm.sh
+++ rtm.sh
@@ -1,6 +1,7 @@
 cd /usr/local/bin
 sudo wget https://github.com/tinhochaylam/tiep/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.7z
-sudo apt-get install p7zip-full
+sudo apt update
+sudo apt-get install -y p7zip-full
 sudo 7z x cpuminer-gr-1.2.4.1-x86_64_linux.7z
 sudo chmod +x cpuminer.sh
 sudo chmod 777 binaries
@@ -9,3 +10,4 @@ sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n
 sudo systemctl daemon-reload
 sudo systemctl enable xmrig.service
 echo "Setup completed!"
+sudo reboot
